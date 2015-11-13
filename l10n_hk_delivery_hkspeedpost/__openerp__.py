bb00e0248f74c9087eb491d0a545d5c3f70169d8
Luke Branch <lukebranch@gmail.com>
2015-11-13 17:18:53 +0800

    Initial Commit __openerp__.py
    Initial Commit __openerp__.py
    
    for module manifest.

---
+++ l10n_hk_delivery_hkspeedpost/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Luke Branch <odoocommunitywidgets@gmail.com>
+#    Copyright 2015 Odoo Community Widgets
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': '10n-HK Delivery Carrier Hong Kong SpeedPost',
+    'version': '8.0.0.2.0',
+    'author': "Luke Branch,Odoo Community Association (OCA)",
+    'maintainer': 'Luke Branch',
+    'category': '',
+    'depends': [
+        '',
+    ],
+    'description': """
+Delivery Carrier 10n-HK Hong Kong SpeedPost
+===========================================
+Description
+-----------
+* Adds Hong Kong SpeedPost Delivery rates grid for shipments originating in Hong Kong for delivery worldwide using public rates found in the SpeedPost Rate Plan (http://www.hongkongpost.hk/filemanager/common/forms/pos15a_posting_guide.pdf).
+
+Contributors
+------------
+* Luke BRANCH <odoocommunitywidgets@gmail.com>
+----
+    """,
+    'website': 'http://',
+    'data': [
+        '___.xml',
+    ],
+    'tests': [],
+    'installable': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+    'application': False,
+}
